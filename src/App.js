910cf8dcb12b558fe8d84c22aa259d763af9d885
Abdul Moiz <88289957+Moiz091@users.noreply.github.com>
2021-08-25 04:38:13 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ import BookSearch from "./BookSearch";
 import BookList from "./BookList";
 import * as BooksAPI from "./BooksAPI";
 
-const BooksApp = ({}) => {
+const BooksApp = () => {
   const [books, setbooks] = useState([]);
   const [searchedBooks, setsearchedBooks] = useState([]);
-  const [filteredBooks, setfilteredBooks] = useState([]);
   const [query, setquery] = useState("");
   useEffect(() => {
     BooksAPI.getAll().then((books) => {
       setbooks(books);
     });
   }, []);
-
+  console.log(query);
   const changeShelf = (book, shelf) => {
     BooksAPI.update(book, shelf).then((response) => {
       book.shelf = shelf;
@@ -31,6 +30,7 @@ const BooksApp = ({}) => {
           if (searchedBooks.error) {
             setsearchedBooks([]);
           } else {
+            // eslint-disable-next-line array-callback-return
             searchedBooks.map((filteredBooks) => {
               let bookOnShelf = books.find((b) => b.id === filteredBooks.id);
 
